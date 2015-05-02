cd11b6774bb87a3fd602d91343458d2ffd7e2069
201313986 Sergio Gerardo García Arteaga <sergio.arteag4@gmail.com>
2015-05-01 23:06:23 -0600

    Practica2

---
+++ Practica2/src/practica.java
@@ -0,0 +1,24 @@
+import java.awt.Container;
+import javax.swing.*;
+
+	class torre {
+		
+		int a1[] = {1,2,3};
+		int a2[] = {1,2,3,4};
+		int a3[] = {1,2,3,4,5};
+		int a4[] = {1,2,3,4,5,6};
+		int a5[] = {1,2,3,4,5,6,7};
+		int a6[] = {1,2,3,4,5,6,7,8};
+		
+		
+		
+	}
+
+public class practica {
+
+	public static void main(String[] args) {
+
+		ventana v = new ventana();
+	}
+
+}
